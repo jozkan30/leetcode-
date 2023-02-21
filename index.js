69ad7fa1779e5523404b3021b0c2cab46688f08f
jozkan30 <jozkan2@gmail.com>
2023-02-21 10:31:39 -0500

    two sums

---
+++ index.js
@@ -0,0 +1,12 @@
+// Sum of two numbers
+
+let myAr =  [4,5,6,7]
+let target = 9
+
+for(let i = 0; i < myAr.length; i++){
+    for(let j = i + 1; j < myAr.length; j++){
+        if(myAr[i] + myAr[j] == target){
+            console.log( myAr[i] , myAr[j])
+        }
+    }
+}
