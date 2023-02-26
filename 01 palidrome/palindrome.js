6ccaed9fa2c10b486d508637bb12dbd547fc7dec
jozkan30 <jozkan2@gmail.com>
2023-02-25 19:53:17 -0500

    palindrome

--- 01 palidrome/palindrome.js
+++ 01 palidrome/palindrome.js
@@ -9,5 +9,25 @@ if(x === word){
     return `Nope! x value: ${x}  reverse value: ${word}`
 }
 }
-
 console.log(isPalindrome("hello"))
+
+
+// If target value is a integer. Must first convert to string.
+
+// let isPalindrome = function (x) {
+//   let num =  x.toString()
+//   let index = num.split('')
+//   let rev = index.reverse()
+//   let word = rev.join('')
+  
+
+//   if(num === word){
+//         return `Match! x value: ${x}  reverse value: ${word}`
+//     }else{
+//         return `Nope! x value: ${x}  reverse value: ${word}`
+//     }
+//     }
+   
+
+console.log(isPalindrome(000))
+
