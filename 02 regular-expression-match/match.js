bdadbe8faa55aefbb8f12e3d5a5ec7b00084ee50
jozkan30 <jozkan2@gmail.com>
2023-03-03 18:18:42 -0500

    push

--- 02 regular-expression-match/match.js
+++ 02 regular-expression-match/match.js
@@ -4,7 +4,6 @@
  * @return {boolean}
  */
 // Great explanation: https://www.youtube.com/watch?v=HAA8mgxlov8&ab_channel=NeetCode
-// TOP DOWN MEMOIZATION SOLUTION
 
 var isMatch = function(s, p) {
     // BRUTE FORCE METHOD - not time efficient 
@@ -19,12 +18,11 @@ var isMatch = function(s, p) {
         }
         
         let match = i < s.length && (s[i] === p[j] || p[j] === '.');
-        
-        // Check if in bounds and whether we have encountered a star
+
         if((j+1) < p.length && p[j+1] === '*') {
-            // recursive call here
-            return check(i, j+2) ||        // don't use *
-            (match && check(i+1, j));      // use *
+            
+            return check(i, j+2) ||        
+            (match && check(i+1, j));      
         }
         
         if(match) {
