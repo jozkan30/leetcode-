5763f06a5c4dd244bda97b7051fa8525f1a48304
jozkan30 <jozkan2@gmail.com>
2023-03-06 17:12:37 -0500

    length of last word with JS

---
+++ 05-length-of-last-string/05.js
@@ -0,0 +1,18 @@
+// 58. Length of Last Word
+
+
+let findLast = (word) =>{
+    let last = ''
+    let arr = word.split(' ')
+    let newArr  =  arr.filter((x)=> x !== '')  
+    let end = newArr.length - 1
+    last = newArr[end]
+    let x =  last.split('')
+    
+    return x.length
+}
+    
+
+console.log(findLast("hello world"))
+console.log(findLast("   fly me   to   the moon  "))
+console.log(findLast("luffy is still joyboy"))
