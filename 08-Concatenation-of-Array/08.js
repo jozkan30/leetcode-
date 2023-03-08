dd96c8b9f75ecd797df998afc358407be1ceeaab
jozkan30 <jozkan2@gmail.com>
2023-03-08 14:11:58 -0500

    concat

--- 08-Concatenation-of-Array/08.js
+++ 08-Concatenation-of-Array/08.js
@@ -1,8 +1,15 @@
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array 
+
+
 var getConcatenation = function(nums) {
     let arr = nums
     let con = arr.concat(...nums)
     return con
 };
 
-console.log(getConcatenation([1,2,1]))
+console.log(getConcatenation([1,2,1])) 
 console.log(getConcatenation([1,3,2,1]))
